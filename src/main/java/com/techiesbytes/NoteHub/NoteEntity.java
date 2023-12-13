a748fce96acb77b17004d0830584d2914ba3fc81
Jainish Prajapati <jainishprajapati98@gmail.com>
2023-12-13 11:27:34 +0530

    Removed Boilerplate code from entity class using project lombok library for Get&Set and Constructors using it's annotations

--- src/main/java/com/techiesbytes/NoteHub/NoteEntity.java
+++ src/main/java/com/techiesbytes/NoteHub/NoteEntity.java
@@ -6,8 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class NoteEntity {
 
     @Id
@@ -19,47 +27,5 @@ public class NoteEntity {
     @NotBlank(message = "What dumb you are. Note without description???")
     private String description;
 
-    public NoteEntity()
-    {
 
-    }
-
-    public NoteEntity(Long id, String heading, String description) {
-        this.id = id;
-        this.heading = heading;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getHeading() {
-        return heading;
-    }
-
-    public void setHeading(String heading) {
-        this.heading = heading;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "NoteEntity{" +
-                "id=" + id +
-                ", heading='" + heading + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
 }
